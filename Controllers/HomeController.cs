243d2662349cd23cf5001c59a33f61620214f433
Twalibu Ismail <itwalibu87@gmail.com>
2024-07-01 21:34:05 +0300

    authorize

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace ConstructApp.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
